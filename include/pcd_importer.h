5b9a957ea4a8fdf26958271e26476f4ea534cc86
Adithya Somashekhar <avsom101@gmail.com>
2025-10-08 20:41:19 -0400

    content folder and added raylib
    I added a folder for any content. Content is essentially anything that is not code. This could be sensor data, point cloud data, 3d models, images, etc. I also added the ray lib library and a raylib test.

--- include/pcd_importer.h
+++ include/pcd_importer.h
@@ -25,3 +25,70 @@
 //    points
 //    data
 //};
+
+void load_pcd_file()
+{
+    file_data         outData;
+    std::stringstream contents;
+    {
+        std::ifstream ifs(path);
+        if (!ifs.is_open()) {
+            std::cerr << "Couldn't find file at path: " << path << "\n";
+            std::exit(3);
+        }
+        contents << ifs.rdbuf();
+    }
+
+    std::string lineBuffer;
+
+    std::string token;
+
+    while (std::getline(contents, lineBuffer)) {
+        std::istringstream lineStream(lineBuffer);
+        lineStream >> token;
+
+        if (token == "v") {
+
+            outData.positions.push_back({});
+            auto &back = outData.positions.back();
+            lineStream >> back.x >> back.y >> back.z >> back.w;
+        }
+
+        if (token == "vt") {
+            outData.texture_coordinates.push_back({});
+            auto &back = outData.texture_coordinates.back();
+            lineStream >> back.u >> back.v >> back.w;
+        }
+
+        if (token == "vn") {
+            outData.normals.push_back({});
+            auto &back = outData.normals.back();
+            lineStream >> back.x >> back.y >> back.z;
+        }
+
+        if (token == "f") {
+            outData.faces.push_back({});
+            auto &back = outData.faces.back();
+            while (lineStream >> token) {
+                auto &vert = back.vertices[back.vertex_count];
+
+
+                std::istringstream splitter(token);
+
+                std::getline(splitter, token, '/');
+                vert.position_index = std::stoul(token);
+
+                if (std::getline(splitter, token, '/')) {
+                    if (!token.empty()) {
+                        vert.texture_coordinates_index = std::stoul(token);
+                    }
+                }
+                if (std::getline(splitter, token, '/')) {
+                    vert.normals_index = std::stoul(token);
+                }
+
+                back.vertex_count += 1;
+            }
+        }
+    }
+}
